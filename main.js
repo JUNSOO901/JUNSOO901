3eb5938c90a10d40903d4fc0d42e29d9d98adca5
JUNSOO901 <96417210+JUNSOO901@users.noreply.github.com>
2022-02-03 16:34:52 +0900

    handle click on "contact me" button on home

--- main.js
+++ main.js
@@ -11,3 +11,26 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar__dark');
     }
 });
+
+// handle scrolling when tapping on the navbar menu
+// 누르면 그 위치로 부드럽게 스크롤링 scrollIntoView() 사용.
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    scrollIntoView(link)
+})
+
+// handle click on "contact me" button on home
+const contactMe = document.querySelector('.home__contact');
+contactMe.addEventListener('click', () => {
+    scrollIntoView('#contact')
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
